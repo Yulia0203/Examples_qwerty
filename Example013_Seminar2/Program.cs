c5d057e07c4f48c6779b42666e3c47c56f51668f
Yulia <mamaeva.jula@gmail.com>
2022-08-23 18:00:49 +0300

    Пример решения задачи из Урока 2_4_с методом

---
+++ Example013_Seminar2/Program.cs
@@ -0,0 +1,20 @@
+﻿  // Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23
+
+  bool CheckNumber(int number)
+  {
+    if(number % 7 == 0)
+    {
+        if(number % 23 == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+  }
+
+  Console.Write("Введите число: ");
+  int firstNumber = Convert.ToInt32(Console.ReadLine());
+  bool result = CheckNumber(firstNumber);
+  Console.Write(result);
+  
+
