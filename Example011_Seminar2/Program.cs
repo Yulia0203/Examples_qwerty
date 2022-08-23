8445291347289a7b83ded3e9bb6e0dfe26c3374c
Yulia <mamaeva.jula@gmail.com>
2022-08-23 11:41:44 +0300

    Пример решения задачи из Урока 2_2

---
+++ Example011_Seminar2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая выводит случаное трехзначное число и удаляет вторую цифру этого числа
+
+Random random = new Random();
+int RandomNumber = random.Next(100, 1000);
+
+Console.WriteLine(RandomNumber);
+
+int firstNumber = RandomNumber / 100;
+int secondNumber = RandomNumber % 100;
+int threeNumber = secondNumber % 10;
+
+int result = (firstNumber * 10) + threeNumber;
+Console.WriteLine(result);
+
+
