060020778944c6a05fb0cdbcaa22be6d5cfa249f
Yulia <mamaeva.jula@gmail.com>
2022-08-23 18:26:06 +0300

    Пример решения задачи из Урока 2_5_с методом_

---
+++ Example014_Seminar2/Program.cs
@@ -0,0 +1,43 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого
+
+
+Console.WriteLine("Введите первое число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+bool CheckNumber(int firstNumber, int secondNumber)
+{
+    if(firstNumber > secondNumber)
+    {
+        if(secondNumber * secondNumber == firstNumber)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    else
+    if(firstNumber * firstNumber == secondNumber)
+    {
+        return true;
+    }
+        else
+            {
+                return false;
+            }
+        }
+    
+
+bool result = CheckNumber (a, b);
+if(result == true)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+  Console.WriteLine("Нет");  
+}
