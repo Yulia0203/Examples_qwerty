202a16cf91a9b1ba892d3a554934e7a151f028af
Yulia <mamaeva.jula@gmail.com>
2022-08-23 17:48:37 +0300

    Пример решения задачи из Урока 2_с методом

--- Example010_ArrayLibrary/Program.cs
+++ Example010_ArrayLibrary/Program.cs
@@ -6,14 +6,31 @@ int randomNumber = random.Next(10, 100);
 
 Console.WriteLine(randomNumber);
 
-int lastNumber = randomNumber % 10;
-int firstNumber = randomNumber / 10;
+int GetMaxNumber(int number)
+{
+    int lastNumber = number % 10;
+    int firstNumber = number / 10;
+    if (lastNumber > firstNumber)
+    {
+        return lastNumber;
+    }
+    else
+    {
+        return firstNumber;
+    }
+}
+int result = GetMaxNumber(randomNumber);
+Console.WriteLine(result);
 
-if(lastNumber > firstNumber)
-{
-    Console.WriteLine("Максимальное число равно " + lastNumber);
-}
-else
-{
-    Console.WriteLine("Максимальное число равно " + firstNumber);
-}
+
+// int lastNumber = randomNumber % 10;
+// int firstNumber = randomNumber / 10;
+
+// if(lastNumber > firstNumber)
+// {
+//     Console.WriteLine("Максимальное число равно " + lastNumber);
+// }
+// else
+// {
+//     Console.WriteLine("Максимальное число равно " + firstNumber);
+// }
