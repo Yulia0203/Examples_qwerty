c6a4508c989b96f1d55b847e9f16bd3b4be6b3d5
Yulia <mamaeva.jula@gmail.com>
2022-08-23 17:08:32 +0300

    Пример решения задачи из Урока 2_3

---
+++ Example012_Seminar2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напиишите программу, которая будет принимать на вход два числа и выводить, 
+//являетмя ли второе число кратным первому. 
+//Если число 2 не кратно 1, то программа выводит отстаток от деления
+
+Console.Write("Введите число ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = firstNumber % secondNumber;
+
+if(result == 0)
+{
+    Console.Write("Кратно");
+}
+else
+{
+    int ostatok = firstNumber % secondNumber;
+    Console.WriteLine(ostatok);
+}
+
+
