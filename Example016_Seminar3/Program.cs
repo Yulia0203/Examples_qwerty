7bdef54645123fe1e8741582a42f6690f88d7d49
Yulia <mamaeva.jula@gmail.com>
2022-08-27 13:58:02 +0300

    Пример решения задачи_2_Семинар 3

---
+++ Example016_Seminar3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напищите программу, которая по заданному номеру четверти показывает диапазон возможных координат точек в этой четверти (x и y)
+
+int Number = Convert.ToInt32(Console.ReadLine());
+string GetCoordinat (int Number)
+{
+    switch(Number)
+    {
+        case 1: return "x > 0, y > 0"; break;
+        case 2: return "x < 0, y > 0"; break;
+        case 3: return "x < 0, y < 0"; break;
+        case 4: return "x > 0, y < 0"; break;
+        default: return("Ошибка"); break;
+    }
+}
+string result = GetCoordinat(Number);
+System.Console.WriteLine(result);
