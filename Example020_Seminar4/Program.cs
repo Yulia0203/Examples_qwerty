bb340a64335598e0f586eabcd334939d7e316b25
Yulia <mamaeva.jula@gmail.com>
2022-09-01 19:17:40 +0300

    Пример решения задачи_2_Семинар 4

---
+++ Example020_Seminar4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе
+// 456 -> 3
+// 78 -> 2
+
+System.Console.WriteLine("Введите число N");
+bool isNumber = int.TryParse(Console.ReadLine(), out int N);
+if (!isNumber)
+{
+    System.Console.WriteLine("Это не число");
+    return;
+}
+
+int GetLenNumber(int number)
+{
+    int count = 0;
+    while (number > 0)
+    {
+        count++;
+        number = number / 10;
+    }
+    return count;
+}
+int result = GetLenNumber(N);
+System.Console.WriteLine(result);
+
