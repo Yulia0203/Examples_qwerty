bf8f8b598be91f48e015ec47fc31f071d7b2492a
Yulia <mamaeva.jula@gmail.com>
2022-09-04 23:15:25 +0300

    Пример решения задачи_1_Семинар 5

---
+++ Example023_Seminar5/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задайте массив из 12 элементов, заполенные случайными числами из промежутка (-9, 9).
+// Найдите сумму отрицательных и положительных элементов массива
+
+Console.WriteLine("Введите длину массива: ");
+
+bool isNumber = int.TryParse(Console.ReadLine(), out int length);
+if (!isNumber || length <= 0)
+{
+    Console.WriteLine("Неверное число");
+    return;
+}
+
+int[] array = FillArray(length);
+PrintArray(array);
+
+int sumPositiveNumber = GetSumPositiveNumberInArray(array);
+Console.WriteLine($"Сумма положительных чисел = {sumPositiveNumber}");
+
+int sumNegativeNumber = GetSumNegativeNumberInArray(array);
+Console.WriteLine($"Сумма отрицательных чисел = {sumNegativeNumber}");
+
+int GetSumNegativeNumberInArray(int[] array)
+{
+    int sumNegativeNumber = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < 0)
+        {
+            sumNegativeNumber += array[i];
+        }
+            
+    }
+    return sumNegativeNumber;
+}
+
+int GetSumPositiveNumberInArray(int[] array)
+{
+    int sumPositiveNumber = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0)
+        {
+            sumPositiveNumber += array[i];
+        }
+            
+    }
+    return sumPositiveNumber;
+}
+
+
+void PrintArray(int[] array)
+{
+    if(array.Length == 0)
+    {
+        Console.WriteLine("Что-то пошло не так");
+        return;
+    }
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]},");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.Write("]");
+}
+
+int[] FillArray(int length)
+{
+    int[]array = new int[length];
+    Random random = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = random.Next(-9, 10);
+    }
+    return array;
+}
