80ba9b28d89fba50d39b389d5b30d3f8213be9c0
Yulia <mamaeva.jula@gmail.com>
2022-09-10 15:01:07 +0300

    Пример решения задачи_1_Семинар 7

---
+++ Example029_Seminar7/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив размером m*n, заполненный случайными целыми числами
+
+int[,] result = CreateRandomArray(5,6);
+Print2DArray(result);
+
+
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateRandomArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = random.Next(0,100);
+        }
+    }
+
+    return array;
+}
+
